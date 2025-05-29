b58f9e4677c35a36483d8fef127329c4360292ab
Kate Miniyanova <katuxa@yandex.ru>
2025-05-29 19:19:49 +0500

    Pixel Perfect

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const navOpenBtn = document.querySelector('.main-nav__toggle');
+const navList = document.querySelector('.main-nav__list');
+
+navOpenBtn.addEventListener('click', function() {
+  this.classList.toggle('main-nav__toggle--active');
+  navList.classList.toggle('main-nav__list--active');
+});
